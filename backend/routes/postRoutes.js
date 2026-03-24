21529b4a00c2b78a373b0d28b2454ef858f0d7b8
Swaraj Gaikwad <116364064+swaraj017@users.noreply.github.com>
2026-03-24 20:06:37 +0530

    Update postRoutes.js

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -9,9 +9,16 @@ import {
 
 const router = express.Router();
 
+ 
 router.post("/", protect, createPost);
-router.get("/", protect, getPosts);
+
+ 
+router.get("/", getPosts);
+
+ 
 router.post("/:id/like", protect, likePost);
+
+ 
 router.post("/:id/comment", protect, commentPost);
 
 export default router;
